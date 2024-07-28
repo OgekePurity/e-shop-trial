95a51d8a4cf2568ec1e79da52f2eb2691b5bd603
Petunia675 <ogekepurity675@gmail.com>
2024-07-28 19:31:44 +0300

    deploy

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,5 @@
-export const server = "https://e-shop-topaz-six.vercel.app/api/v2";
+export const server = "https://e-shop-aj1t.vercel.app/api/v2";
 
-export const backend_url = "https://e-shop-topaz-six.vercel.app/";
+export const backend_url = "https://e-shop-aj1t.vercel.app/";
 
 
